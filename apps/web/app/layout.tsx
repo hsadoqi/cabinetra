1eafbbdd88f49df469db63dcf084afed548ab478
Hanaa Sadoqi <hsadoqi@gmail.com>
2026-04-26 01:11:37 +0100

    feat(ui): add root Providers component and integrate it into RootLayout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+
+import type { Metadata } from "next";
+import { Providers } from "./providers";
+import localFont from "next/font/local";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Providers>
         {children}
+        </Providers>
       </body>
     </html>
   );
