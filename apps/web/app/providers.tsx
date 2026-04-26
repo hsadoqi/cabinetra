6be0820ecfbad2f8bea85c21a12fb407ed1eac70
Hanaa Sadoqi <hsadoqi@gmail.com>
2026-04-26 15:04:09 +0100

    refactor: enhance providers by consolidating imports and updating tsconfig; improve turbo.json outputs

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -3,11 +3,8 @@
 import React from "react"
 import { I18nProvider, type Locale } from "@cabinetra/platform-i18n"
 import { SidebarProvider } from "@cabinetra/ui-components"
-import { ThemeProvider, type Theme } from "@cabinetra/ui-components/providers"
+import { ThemeProvider, type Theme, CommandProvider, ToastProvider } from "@cabinetra/ui-components/providers"
 import { TooltipProvider } from "@cabinetra/ui-components/primitives"
-
-import { CommandProvider } from "@cabinetra/ui-components/providers"
-import { ToastProvider } from "../../../packages/ui/components/src/providers/toast-provider"
 import { ClientProvider } from "@cabinetra/ui-clients"
 import { ClientSwitcherModalProvider } from "@/lib/client-switcher-context"
 
